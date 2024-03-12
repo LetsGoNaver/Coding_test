23a96dc8d1823e0867ceb72e4d0fe8586311d500
jaeyo03 <toyazx@naver.com>
2024-03-13 06:25:51 +0900

    지피티의 정답 추가

--- 2024KakaoWinterIntern/Dice_Lv3.py
+++ 2024KakaoWinterIntern/Dice_Lv3.py
@@ -69,4 +69,54 @@ def solution(dice):
 
     
 
-        
+
+# GPT 의 정답 - 내 코드는 시간 복잡도에서 문제가 생겼다 
+
+from itertools import combinations, product
+from collections import defaultdict
+
+def calculate_distribution(dice):
+    dist = defaultdict(int)
+    for value in dice:
+        dist[value] += 1
+    return dist
+
+def combine_distributions(dist1, dist2):
+    combined = defaultdict(int)
+    for val1, count1 in dist1.items():
+        for val2, count2 in dist2.items():
+            combined[val1 + val2] += count1 * count2
+    return combined
+
+def solution(dice):
+    n = len(dice)
+    half_n = n // 2
+    max_win_probability = -1
+    best_combination = []
+
+    # 각 주사위의 점수 분포를 계산
+    distributions = [calculate_distribution(d) for d in dice]
+
+    # 모든 가능한 주사위 조합을 생성
+    for dice_a in combinations(range(n), half_n):
+        dist_a = defaultdict(int, {0: 1})
+        dist_b = defaultdict(int, {0: 1})
+
+        for d in dice_a:
+            dist_a = combine_distributions(dist_a, distributions[d])
+        for d in set(range(n)) - set(dice_a):
+            dist_b = combine_distributions(dist_b, distributions[d])
+
+        # 승리 확률 계산
+        total_cases = sum(dist_a.values()) * sum(dist_b.values())
+        wins = sum(count_a * sum(count_b for key_b, count_b in dist_b.items() if key_b < key_a)
+                   for key_a, count_a in dist_a.items())
+
+        win_probability = wins / total_cases
+
+        if win_probability > max_win_probability:
+            max_win_probability = win_probability
+            best_combination = dice_a
+
+    # 주사위 번호에 1을 더함
+    return [x + 1 for x in sorted(best_combination)]
