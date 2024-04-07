4edc4c4b823cb24fccc9af335c6ba6428aac47ac
jaeyo03 <toyazx@naver.com>
2024-04-08 01:57:08 +0900

    백준문제풀이

--- Samsung/Baekjoon_20057.py
+++ Samsung/Baekjoon_20057.py
@@ -3,13 +3,13 @@ graph = []
 for _ in range(n):
     a = list(map(int,input().split()))
     graph.append(a)
-#왼쪽 : 두번째 -1
-#아래 : 첫번째 +1
-#오른쪽 : 두번째 +1
-#위 : 첫번째 -1
-
 
 def solution(n,graph):
+    def printing():
+        print("----------------")
+        for i in graph:
+            print(i)
+        print("----------------")
     answer = 0
     m = int((n-1)/2)
     start = [m,m]
@@ -18,12 +18,12 @@ def solution(n,graph):
     
     
     moving = [
-        [(-2,0),(-1,-1), (-1,0), (-1,1),(0,-2), (1,-1),(1,0),(1,1),(2,0),(0,-1)],
-     [(0,-2),(1,-1), (0,-1), (-1,-1),(2,0), (1,1),(0,1),(-1,1),(0,2),(1,0)],
-     [(2,0),(1,1), (1,0), (1,-1),(0,2), (-1,1),(-1,0),(-1,-1),(-2,0),(0,1)],
-     [(0,2),(-1,1), (0,1), (1,1),(-2,0), (-1,-1),(0,-1),(1,-1),(0,-2),(-1,0)]
+        [(-1,1),(1,1),(2,0),(-2,0),(-1,0),(1,0),(-1,-1),(1,-1),(0,-2),(0,-1)],
+        [(-1,-1),(1,-1),(2,0),(-2,0),(-1,0),(1,0),(-1,1),(1,1),(0,2),(0,1)],
+        [(1,-1),(1,1),(0,-1),(0,1),(2,0),(-2,0),(-1,-1),(-1,1),(-2,0),(-1,0)],
+        [(-1,-1),(-1,1),(0,-1),(0,1),(2,0),(-2,0),(1,-1),(1,1),(2,0),(1,0)]
      ]
-    percentage =[0.01,0.01,0.02,0.02,0.07,0.07,0.1,0.1,0.05,0.65]
+    percentage =[0.01,0.01,0.02,0.02,0.07,0.07,0.1,0.1,0.05,1] # 마지막에서는 전체에서 - 날라간양
 
     def is_out(x,y):
         if x < 0 or x >= n or y < 0 or y >= n:
@@ -34,32 +34,67 @@ def solution(n,graph):
         depth = point[0]
         x = point[1]
         blown = 0
-        if graph[depth,x] != 0:
-            if direction == "left":
-                inx = d.index(direction)
-                for i in range(10):
-                    depth += moving[inx][0]
-                    x += moving[inx][1]
+        if graph[depth][x] != 0:
+            inx = d.index(direction)
+            moved_sand = 0
+            for i in range(10):
+                td = point[0]
+                tx = point[1]
+                sand = int(graph[td][tx]*percentage[i])
+                moved_sand += sand
+                td += moving[inx][i][0]
+                tx += moving[inx][i][1]
+                if is_out(td,tx):
+                    blown += sand
+                else:
+                    graph[td][tx] += sand
+                
+                if i == 9:
+                    td = point[0]
+                    tx = point[1]
+                    sand = graph[td][tx] - moved_sand
+                    td += moving[inx][i][0]
+                    tx += moving[inx][i][1]
+                    if is_out(td,tx):
+                        blown += sand
+                    else:
+                        graph[td][tx] += sand
+
+            graph[depth][x] = 0
                 
         return blown
 
     for i in range(1,n):
         if i%2 != 0:
-            start[1] -= i
-            arrow = "left"
-            blow(start,arrow)
-            start[0] += i
-            arrow = "down"
-            blow(start,arrow)
+            for _ in range(i):
+                start[1] -= 1
+                arrow = "left"
+                answer += blow(start,arrow)
+                printing()
+            for _ in range(i):
+                start[0] += 1
+                arrow = "down"
+                answer += blow(start,arrow)
+                printing()
         else:
-            start[1] += i
-            arrow = "right"
-            blow(start,arrow)
-            start[0] -= i
-            arrow = "up"
-            blow(start,arrow)
+            for _ in range(i):
+                start[1] += 1
+                arrow = "right"
+                answer += blow(start,arrow)
+                printing()
+            for _ in range(i):
+                start[0] -= 1
+                arrow = "up"
+                answer += blow(start,arrow)
+                printing()
 
-    start[1] -= n-1
-    arrow = "left"
-    blow(start,arrow)
+    for _ in range(n-1):
+        start[1] -= 1
+        arrow = "left"
+        answer += blow(start,arrow)
+        printing()
     return answer
+
+print("answer is",solution(n,graph))
+
+
