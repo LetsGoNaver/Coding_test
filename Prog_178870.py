1a9028cdc92af865fa610d3676b08a462bd4d979
jaeyo03 <toyazx@naver.com>
2024-04-28 23:00:36 +0900

    프로그래머스 문제풀이

---
+++ Prog_178870.py
@@ -0,0 +1,19 @@
+def solution(sequence, k):
+    answer = []
+    count = 0
+    interval_sum = 0
+    end = 0
+    n = len(sequence)
+    # start를 차례대로 증가시키며 반복
+    for start in range(n):
+        # end를 가능한 만큼 이동시키기
+        while interval_sum < k and end < n:
+            interval_sum += sequence[end]
+            end += 1
+        # 부분합이 m일 때 카운트 증가
+        if interval_sum == k:
+            answer.append([start,end-1,end-1-start])
+        interval_sum -= sequence[start]
+        
+    answer.sort(key = lambda x:x[2])
+    return answer[0][0:2]
